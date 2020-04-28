6b404eebd35672e590909744a27b60f5aef34d17
sthendric <stefan.hendricks@googlemail.com>
2020-04-28 14:57:12 +0200

    reorganize basic tests

--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -38,6 +38,22 @@ class BasicFunctionalityTestSuite(unittest.TestCase):
         self.assertEqual(prd.tcs.dt, tcs_reference_dt)
         self.assertEqual(prd.tce.dt, tce_reference_dt)
 
+    def test_netcdf_attribute_dict(self):
+        prd = dateperiods.DatePeriod([2018, 4, 1], [2018, 4, 1])
+        attr_dict = prd.get_netcdf_attributes(zulu=True)
+        self.assertTrue("time_coverage_start" in attr_dict)
+        self.assertEqual(attr_dict["time_coverage_start"], "20180401T000000Z")
+        self.assertTrue("time_coverage_end" in attr_dict)
+        self.assertEqual(attr_dict["time_coverage_end"], "20180401T235959Z")
+        self.assertTrue("time_coverage_duration" in attr_dict)
+        self.assertEqual(attr_dict["time_coverage_duration"], "P1D")
+        self.assertTrue("time_coverage_resolution" in attr_dict)
+        self.assertEqual(attr_dict["time_coverage_resolution"], "P1D")
+
+
+class DurationFunctionalityTestSuite(unittest.TestCase):
+    """A standard list of test cases """
+
     def test_duration_year(self):
         prd = dateperiods.DatePeriod([2018], [2018])
         duration = prd.duration
@@ -88,41 +104,7 @@ class BasicFunctionalityTestSuite(unittest.TestCase):
         self.assertEqual(duration.isoformat, "P2D")
         self.assertEqual(duration.type, "custom")
 
-    def test_netcdf_attribute_dict(self):
-        prd = dateperiods.DatePeriod([2018, 4, 1], [2018, 4, 1])
-        attr_dict = prd.get_netcdf_attributes(zulu=True)
-        self.assertTrue("time_coverage_start" in attr_dict)
-        self.assertEqual(attr_dict["time_coverage_start"], "20180401T000000Z")
-        self.assertTrue("time_coverage_end" in attr_dict)
-        self.assertEqual(attr_dict["time_coverage_end"], "20180401T235959Z")
-        self.assertTrue("time_coverage_duration" in attr_dict)
-        self.assertEqual(attr_dict["time_coverage_duration"], "P1D")
-        self.assertTrue("time_coverage_resolution" in attr_dict)
-        self.assertEqual(attr_dict["time_coverage_resolution"], "P1D")
 
-    # def test_daily_segmentation(self):
-    #     prd = dateperiods.DatePeriod([2018, 4], [2018, 4])
-    #     prds = prd.get_period_segments("daily")
-    #     self.assertEqual(len(prds), 30)
-    #     prds = prd.get_period_segments("daily", exclude_month=[5, 6, 7, 8, 9])
-    #     self.assertEqual(len(prds), 30)
-    #     self.assertEqual(prds[0].period_type, "daily")
-    #
-    # def test_default_week_segmentation(self):
-    #     prd = dateperiods.DatePeriod([2017, 4], [2017, 10])
-    #     prds = prd.get_period_segments("default_week")
-    #     self.assertEqual(len(prds), 32)
-    #     prds = prd.get_period_segments("default_week", exclude_month=[5, 6, 7, 8, 9])
-    #     self.assertEqual(len(prds), 11)
-    #     self.assertEqual(prds[0].period_type, "default_week")
-    #
-    # def test_monthly_segmentation(self):
-    #     prd = dateperiods.DatePeriod([2017, 4], [2018, 4])
-    #     prds = prd.get_period_segments("monthly")
-    #     self.assertEqual(len(prds), 13)
-    #     prds = prd.get_period_segments("monthly", exclude_month=[5, 6, 7, 8, 9])
-    #     self.assertEqual(len(prds), 8)
-    #     self.assertEqual(prds[0].period_type, "monthly")
 class OverlapFunctionalityTestSuite(unittest.TestCase):
     """A standard list of test cases """
 
