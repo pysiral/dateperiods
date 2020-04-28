9abbcd6b9b86b17468eee2eaca63567b9a43c109
sthendric <stefan.hendricks@googlemail.com>
2020-04-28 12:53:10 +0200

    add iterator tests

--- tests/test_segmentation.py
+++ tests/test_segmentation.py
@@ -18,10 +18,34 @@ class SegmentationFunctionalityTestSuite(unittest.TestCase):
         self.assertRaises(ValueError, prd.get_segments, "a")
         self.assertRaises(ValueError, prd.get_segments, 1)
 
+    def test_segment_iterator(self):
+        prd = dateperiods.DatePeriod([2018, 4], [2018, 4])
+        segments = prd.get_segments("day")
+        for segment in segments:
+            self.assertIsInstance(segment, dateperiods.DatePeriod)
+
+    def test_segment_lengths_days(self):
+        prd = dateperiods.DatePeriod([2018, 4], [2018, 4])
+        segments = prd.get_segments("day")
+        self.assertEqual(segments.n_periods, 30)
+        for segment in segments:
+            self.assertEqual(segment.tcs.date, segment.tce.date)
+
+    def test_segment_lengths_isoweeks(self):
+        prd = dateperiods.DatePeriod([2018, 4], [2018, 4])
+        segments = prd.get_segments("isoweek")
+        self.assertEqual(segments.n_periods, 5)
+        for segment in segments:
+            self.assertTrue(segment.tcs.is_monday)
+            self.assertTrue(segment.tce.is_sunday)
+
     def test_segment_lengths_months(self):
         prd = dateperiods.DatePeriod([2018, 3], [2018, 4])
-        prds = prd.get_segments("month")
-        self.assertEqual(prds.n_periods, 2)
+        segments = prd.get_segments("month")
+        self.assertEqual(segments.n_periods, 2)
+        for segment in segments:
+            self.assertEqual(segment.tcs.dt.day, 1)
+            self.assertTrue(segment.tce.is_last_day_of_month)
 
 
 if __name__ == '__main__':
