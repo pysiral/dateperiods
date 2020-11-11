f53a4d32d0aa5d1d424ef789a70246a464005e7e
Stefan Hendricks <stefan.hendricks@awi.de>
2020-11-11 14:12:35 +0100

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='dateperiods',
-    version='1.0',
+    version='1.0.1',
     description='Managing date-based periods in python',
     long_description=readme,
     author='Stefan Hendricks',
