e8e7f8ff99c8a40a83046f7e66aff24b3dbdedf6
Stefan Hendricks <stefan.hendricks@awi.de>
2022-02-07 13:07:58 +0100

    Update setup.py
    Update supported pyton versions

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8']
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9']
     )
